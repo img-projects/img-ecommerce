005a520beaa4338dfe048290232dab60ca6b4777
preetam.img <99808975+preetamimg@users.noreply.github.com>
2023-01-31 17:05:03 +0530

    worked on header

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,31 @@
-$(function(){
-    $("#header").load("layouts/header.html"); 
-    $("#models").load("layouts/models.html"); 
-    // $("#footer").load("footer.html"); 
-});
+// header js
+let subCategoryLis = document.querySelectorAll('.subCategoryLi');
+let innerCatBoxs = document.querySelectorAll('.innerCatBox');
+subCategoryLis.forEach(subCategoryLi => {
+    subCategoryLi.addEventListener('mouseover', (e) => {
+        for(i=0; i<subCategoryLis.length; i++){
+            subCategoryLis[i].classList.remove('active')
+        }
+        for(i=0; i<innerCatBoxs.length; i++){
+            innerCatBoxs[i].classList.remove('active')
+        }
+        let a = e.currentTarget.getAttribute("cat-sec");
+        let b = document.getElementById(a);
+        b.classList.add('active');
+        e.currentTarget.classList.add('active')
+    })
+})
+
+// hero carousel js
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
