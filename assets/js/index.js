d72d3713e91ca19b0e93c210c78cf1c24b57fe10
preetam.img <99808975+preetamimg@users.noreply.github.com>
2023-02-03 17:13:15 +0530

    worked on home page

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,7 @@ subCategoryLis.forEach(subCategoryLi => {
 })
 
 // hero carousel js
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".heroSwiper", {
     spaceBetween: 30,
     effect: "fade",
     autoplay: {
@@ -37,7 +37,7 @@ var swiper = new Swiper(".mySwiper", {
 
   // product preview carousel
 
-    var swiper = new Swiper(".productthumbCarousel", {
+    var swiper1 = new Swiper(".productthumbCarousel", {
       loop: true,
       spaceBetween: 10,
       slidesPerView: 4,
@@ -52,6 +52,76 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
       },
       thumbs: {
-        swiper: swiper,
+        swiper: swiper1,
       },
     });
+
+
+    // product Category carousel
+
+    var swiper5 = new Swiper(".productCategorySlider", {
+      slidesPerView: 1,
+      loop: true,
+      spaceBetween: 24,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 2,
+          spaceBetween: 16,
+        },
+        576: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        992: {
+          slidesPerView: 2,
+        },
+        1200: {
+          slidesPerView: 3,
+        },
+        1400: {
+          slidesPerView: 3,
+        },
+      },
+    });
+
+
+    // mobile category slider
+    var swiper = new Swiper(".mobCategorySlider", {
+      slidesPerView: 1,
+      loop: true,
+      spaceBetween: 8,
+      breakpoints: {
+        0: {
+          slidesPerView: 5,
+        },
+        500: {
+          slidesPerView: 6,
+        },
+        600: {
+          slidesPerView: 7,
+        },
+        700: {
+          slidesPerView: 8,
+        },
+        800: {
+          slidesPerView: 9,
+        },
+        900: {
+          slidesPerView: 10,
+        },
+      },
+    });
+
+
+    // preloader
+    $(window).on('load', function () {
+      setTimeout(function () {
+          $('.loader').fadeOut('active');
+      }, 100);
+  });
